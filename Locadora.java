af752c453707bc6eaa60a4d28547d546d456ab47
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-04-04 17:22:42 +0000

    salvo

---
+++ Locadora.java
@@ -0,0 +1,12 @@
+public class Locadora
+{
+
+    public static void main(String[] args)
+    {
+      int primeiraSemana =4 , segundaSemana=5, terceiraSemana=2, quartaSemana=7, total;
+
+       total = primeiraSemana + segundaSemana + terceiraSemana + quartaSemana;
+
+      System.out.println("O filme foi alugado um total de  "+ total);
+    }
+}
